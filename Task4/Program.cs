d2f5d9814da52aafd3d01bbc375b1555742ccc8c
Dmitriy <trayan@yandex.ru>
2023-06-19 17:00:50 +1100

    Task4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine("Введите поочередно три различных целых числа");
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = int.Parse(Console.ReadLine());
+int nummax = 0;
+if(num1 > num2)
+{
+    nummax = num1;
+}
+else
+{
+    nummax = num2;
+}
+if(nummax > num3)
+{
+    Console.WriteLine($"Максимальное число - {nummax}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число - {num3}");
+}
