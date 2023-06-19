31457ee3902c889792e409ff1a9964168aa8a363
Dmitriy <trayan@yandex.ru>
2023-06-19 17:21:03 +1100

    Task8 added

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число больше 1");
+int num=int.Parse(Console.ReadLine());
+int count = 1;
+while(count<=num)
+{
+    if(count%2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+count++;
+}
