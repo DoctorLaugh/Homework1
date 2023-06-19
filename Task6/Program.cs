6ff93c729dd1c956758efb31ab131b3b015006f0
Dmitriy <trayan@yandex.ru>
2023-06-19 17:12:19 +1100

    Task6 added

---
+++ Task6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число");
+int num = int.Parse(Console.ReadLine());
+if(num%2 == 0) Console.WriteLine("Число чётное");
+else Console.WriteLine("Число нечётное");
